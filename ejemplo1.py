0800097f1aff98a2773679b6098cd5a829121e60
leandroleonardo88 <leandro.leonardo2@gmail.com>
2021-06-28 18:51:22 -0300

    primera version de prueba

---
+++ ejemplo1.py
@@ -0,0 +1,8 @@
+num1 = int(input("Ingrese el primer valor"))
+num2 = int(input("Ingrese el segundo valor"))
+
+suma = num1 + num2
+producto = num1*num2
+
+print("la suma de los dos numeros es", suma)
+print("el producto de los dos numeros es", producto)

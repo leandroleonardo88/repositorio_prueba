4cd918bcb8aa95d5bc6680406964902893f991c3
leandroleonardo88 <leandro.leonardo2@gmail.com>
2021-06-28 19:10:26 -0300

    Create ejemplo2.py

---
+++ ejemplo2.py
@@ -0,0 +1,7 @@
+edad1 = int(input("Ingrese el primer valor"))
+edad2 = int(input("Ingrese el segundo valor"))
+
+if edad1 > edad2:
+    print("el mas viejo es",edad1)
+else:
+    print("el mas viejo es",edad2)
